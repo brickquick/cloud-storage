c5f2247125d38cce6e880e9d45c8c95624be9330
dmr <brickdamik1996@gmail.com>
2021-09-23 16:28:39 +0300

    hw1

--- server/src/main/java/qbrick/Handler.java
+++ server/src/main/java/qbrick/Handler.java
@@ -1,7 +1,10 @@
 package qbrick;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
@@ -9,7 +12,12 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Handler implements Runnable {
 
+    private static final int BUFFER_SIZE = 256;
+    private static final String ROOT_DIR = "server/root/";
+    private final byte[] buffer = new byte[BUFFER_SIZE];
     private final Socket socket;
+    private DataOutputStream os;
+    private DataInputStream is;
 
     public Handler(Socket socket) {
         this.socket = socket;
@@ -22,16 +30,30 @@ public class Handler implements Runnable {
     @SneakyThrows
     @Override
     public void run() {
-        try (DataOutputStream os = new DataOutputStream(socket.getOutputStream());
-             DataInputStream is = new DataInputStream(socket.getInputStream())) {
+        try  {
+            is = new DataInputStream(socket.getInputStream());
+            os = new DataOutputStream(socket.getOutputStream());
             while (true) {
-                String s = is.readUTF();
-                log.debug("Received: {}", s);
-                os.writeUTF(s);
-                os.flush();
+                String fileName = is.readUTF();
+                log.debug("Received fileName: {}", fileName);
+                long size = is.readLong();
+                log.debug("File size: {}", size);
+                int read;
+                try (OutputStream fos = Files.newOutputStream(Paths.get(ROOT_DIR, fileName))) {
+                    for (int i = 0; i < (size + BUFFER_SIZE - 1) / BUFFER_SIZE; i++) {
+                        read = is.read(buffer);
+                        fos.write(buffer, 0 , read);
+                    }
+                } catch (Exception e) {
+                    log.error("problem with file system");
+                }
+                os.writeUTF("OK");
             }
         } catch (Exception e) {
             log.error("stacktrace: ", e);
+            os.writeUTF("Error! " + e.getMessage());
         }
+        is.close();
+        os.close();
     }
 }
